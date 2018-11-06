390acc19d32d9a64b57642b0d4bf841fca421fca
EpicMovie <superpowerex@naver.com>
2018-11-06 20:12:51 +0900

    Update SmartPointer.cpp

--- Sandbox/SmartPointer.cpp
+++ Sandbox/SmartPointer.cpp
@@ -141,5 +141,85 @@ int main()
         std::cout << "     " << v3[i] << '\n';
 }
 
+/******************************************************************************
+std::shared_ptr
 
+#include <memory>
+
+template <typename T> class shared_ptr;
+
+std::shared_ptr is a smart pointer that retains shared ownership of an object through a pointer
+
+Several shared_ptr objects may own the same object
+
+The object is destroyed and its memory deallocated when either of the following happens
+
+1. the last reamining shared)ptr owning the object is destroyed
+
+2. the last remaining shared_ptr owning the object is assigned another pointer via operator= or reset()
+
+*/
+
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+struct Base
+{
+    Base() { std::cout << "Base::Base()" << std::endl; }
+    ~Base() { std::cout << "Base::~Base()" << std::endl; }
+};
+
+struct Derived : public Base
+{
+    Derived() { std::cout << "Derived::Derived()" << std::endl; }
+    ~Derived() { std::cout << "Derived::~Derived()" << std::endl; }
+};
+
+struct Third : public Base, public std::enable_shared_from_this<Third> 
+{
+};
+
+void thr(std::shared_ptr<Base> p)
+{
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::shared_ptr<Base> lp = p;
+    
+    {
+        static std::mutex io_mutex;
+        std::lock_guard<std::mutex> lk(io_mutex);
+        std::cout << "Local pointer in a thread : " << std::endl;
+        std::cout << " lp.get() = " << lp.get() 
+                  << " lp.use_count() = " << lp.use_count() << std::endl;
+    }
+}
+
+int main()
+{
+    auto del = [](Base* InBase)
+    {
+        std::cout << "Q is removed" << std::endl;
+        delete InBase; 
+    };
+    
+    std::shared_ptr<Base> p = std::make_shared<Derived>();
+    
+    {
+        std::shared_ptr<Base> q (new Base, del);    
+    }
+    
+    std::cout << "Created a shared Derived (as a pointer to Base)\n"
+              << "  p.get() = " << p.get()
+              << ", p.use_count() = " << p.use_count() << '\n';
+    std::thread t1(thr, p), t2(thr, p), t3(thr, p);
+    p.reset(); // release ownership from main
+    std::cout << "Shared ownership between 3 threads and released\n"
+              << "ownership from main:\n"
+              << "  p.get() = " << p.get()
+              << ", p.use_count() = " << p.use_count() << '\n';
+    t1.join(); t2.join(); t3.join();
+    std::cout << "All threads completed, the last one deleted Derived\n";   
+}
 
